bbc3a54c599c528271d84342bf7385f146ad7ed4
Nikita Andriyanov <nikitos@itws.ru>
2017-12-19 14:33:22 +0300

    Add multimodel select field

---
+++ netprofile_core/netprofile_core/static/webshell/form/field/SimpleMultiModelSelect.js
@@ -0,0 +1,58 @@
+/**
+ * @class NetProfile.form.field.SimpleMultiModelSelect
+ * @extends Ext.form.field.Tag
+ */
+
+Ext.define('NetProfile.form.field.SimpleMultiModelSelect', {
+    extend: 'Ext.form.field.Tag',
+    alias: 'widget.simplemultimodelselect',
+    requires: [
+    ],
+
+    config: {
+            apiModule: null,
+            apiClass: null,
+            hiddenField: null,
+            extraParams: null,
+
+            editable: false,
+            multiField: true,
+            valueField: '__str__',
+            displayField: '__str__',
+            selectOnFocus: false,
+
+            showLink: false,
+    },
+
+    initComponent: function()
+    {
+            if(!this.store)
+            {
+                    this.store = NetProfile.StoreManager.getStore(
+                            this.apiModule,
+                            this.apiClass,
+                            null, true,
+                            this.extraParams
+                    );
+            }
+            this.callParent(arguments);
+            this.on('select', function(cbox, recs, opt)
+            {
+                    if(!this.hiddenField)
+                            return;
+                    var form = this.up('form'),
+                            hf = form.down('field[name=' + this.hiddenField + ']');
+
+                    if(!hf)
+                            return;
+                    if(recs)
+                    {
+                            var uids = [];
+                            for (var i = 0; i < recs.length; i++) {
+                                    uids.push(recs[i].getId());
+                            }
+                            hf.setValue(JSON.stringify(uids));
+                    }
+            }, this);
+    },
+});
